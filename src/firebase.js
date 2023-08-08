104812a8e11a3eb9f681ec7e8cb7b12defe60bbb
Piyaporn <piyapornlahr@gmail.com>
2023-08-08 17:37:28 +0100

    Added firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBG9he9a_mFYmCeHgebCYloFEhGk_vUyOw",
+  authDomain: "expense-manager-frontend-e1ae8.firebaseapp.com",
+  projectId: "expense-manager-frontend-e1ae8",
+  storageBucket: "expense-manager-frontend-e1ae8.appspot.com",
+  messagingSenderId: "494410298449",
+  appId: "1:494410298449:web:5b72d2eeea3393cbae7470"
+};
+
+const app = initializeApp(firebaseConfig);
