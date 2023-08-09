aef4f934e3f1d363edd789f2ba0925cfc4110400
Piyaporn <piyapornlahr@gmail.com>
2023-08-09 14:42:49 +0100

    Weekly bar chart added to home page

---
+++ src/components/WeeklyGraph/WeeklyGraph.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./WeeklyGraph.scss";
+import { Bar } from "react-chartjs-2";
+import { Chart as ChartJs } from "chart.js/auto";
+
+const WeeklyGraph = ({ chartData }) => {
+  return (
+    <div className="weeklyGraph">
+      <Bar data={chartData} />
+    </div>
+  );
+};
+
+export default WeeklyGraph;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.scss";
 import Heading from "../../components/Heading/Heading";
 import NavBar from "../../components/NavBar/NavBar";
 import WelcomeUser from "../../components/WelcomeUser/WelcomeUser";
 import TotalSpend from "../../components/TotalSpend/TotalSpend";
+import WeeklyGraph from "../../components/WeeklyGraph/WeeklyGraph";
 
 const Home = () => {
+  const chartData = [45, 30, 12, 55, 70, 20, 30];
+
+  const [data, setData] = useState({
+    labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    datasets: [
+      {
+        label: "Total spend this week",
+        data: chartData,
+        backgroundColor: "rgba(178, 174, 174, 0.7)",
+        barPercentage: 0.5,
+        hoverBackgroundColor: "#FFBF00",
+      },
+    ],
+  });
+
   return (
     <div className="home-page">
       <div>
         <Heading title="Home" />
       </div>
-      <div>
+      <div className="home-page__container">
         <WelcomeUser />
         <TotalSpend />
+        <div>
+          <WeeklyGraph chartData={data} />
+        </div>
       </div>
       <div>
         <NavBar />
